d629674d1c930ce44e861dd61603d25fd7a64361
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2025-09-01 16:37:16 +0200

    add sse server

--- relay/main.go
+++ relay/main.go
@@ -1,5 +1,28 @@
-package relay
+package main
 
-import "github.com/fibonacid/holeworm/common"
+import (
+	"github.com/fibonacid/holeworm/common"
+	"github.com/tmaxmax/go-sse"
+	"log"
+	"net/http"
+	"time"
+)
 
 var t common.Transfer
+
+func main() {
+	s := &sse.Server{}
+
+	go func() {
+		m := &sse.Message{}
+		m.AppendData("Hello world")
+
+		for range time.Tick(time.Second) {
+			_ = s.Publish(m)
+		}
+	}()
+
+	if err := http.ListenAndServe(":8000", s); err != nil {
+		log.Fatalln(err)
+	}
+}
